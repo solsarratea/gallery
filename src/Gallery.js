10c104e820092c8f184f0dc71439a4bf7b8fe313
Danielle Hoo <daniellem.hoo@gmail.com>
2020-04-21 11:14:09 -0400

    updated readme + created gallery for individual artwork

--- src/Gallery.js
+++ src/Gallery.js
@@ -2,33 +2,49 @@ import React, { Component } from "react";
 import * as THREE from "three";
 const OrbitControls = require("three-orbit-controls")(THREE);
 
+// to do:
+// 1. restrict zoom
+// 2. animate each piece within an individual gallery
+
 class Shape extends Component {
     constructor(props) {
       super(props);
       this.animate = this.animate.bind(this);
       this.addCube = this.addCube.bind(this);
       this.initializeCamera = this.initializeCamera.bind(this);
+      
       this.initializeOrbits = this.initializeOrbits.bind(this);
     }
   componentDidMount() {
-      const width = this.mount.clientWidth;
-      const height = this.mount.clientHeight;
+      const width = window.innerWidth;
+      const height = window.innerHeight;
       this.scene = new THREE.Scene();
-      this.camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
+      this.camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 2000);
       this.renderer = new THREE.WebGLRenderer({ antialias: true });
       this.controls = new OrbitControls(this.camera, this.renderer.domElement);
       this.renderer.setSize(width, height);
       this.mount.appendChild(this.renderer.domElement);
       this.initializeOrbits();
       this.initializeCamera();
+
+      this.camera.lookAt(this.scene.position);	
+  
       
-      var geometry = new THREE.BoxBufferGeometry( 1, 1, 1);
+      
+      var geometry = new THREE.CubeGeometry( 500, 500, 500 );	
       var texture = new THREE.TextureLoader().load("images/ten_largest_no3.jpg");
-      console.log(texture)
-      var material = new THREE.MeshBasicMaterial( {
-        map: texture
-     } );
-      this.cube = new THREE.Mesh( geometry, material );
+    
+      var materialArray = [];
+      for (var i = 0; i < 6; i++)
+        materialArray.push( new THREE.MeshBasicMaterial({
+          map: texture,
+          side: THREE.BackSide
+        }));
+      var galleryMaterial = new THREE.MeshFaceMaterial( materialArray );
+    
+
+  
+      this.cube = new THREE.Mesh( geometry, galleryMaterial );
       this.scene.add( this.cube );
       this.animate();
     }
@@ -43,8 +59,8 @@ class Shape extends Component {
     }
   initializeCamera() {
       this.camera.position.x = 0;
-      this.camera.position.y = 0;
-      this.camera.position.z = 4;
+      this.camera.position.y = 150;
+      this.camera.position.z = 400;
     }
   animate() {
       this.frameId = window.requestAnimationFrame(this.animate);
